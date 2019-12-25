1bfc3142a0302aa2d17bff1bcc2d6addee84fc03
zhangyu <grewer@grewer.cn>
2019-12-26 00:16:26 +0800

    test(*): test 特殊情况

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,7 +6,7 @@ const storage = new webStorage({
   preId
 })
 
-describe('Normal setting',()=>{
+describe('Normal setting', () => {
   test('set value', () => {
     localStorage.clear();
 
@@ -35,5 +35,47 @@ describe('Normal value, delete value', () => {
     storage.remove('gre')
     const ins = storage.get('gre')
     expect(ins.value).toEqual(null);
+  })
+})
+
+describe('Special case', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  test('get value', () => {
+    const ins = storage.get('gre')
+    expect(ins.value).toEqual(null);
+    expect(ins.status).toEqual(1);
+  })
+
+  test('remove value', () => {
+    const ins = storage.remove('gre')
+    expect(ins.status).toEqual(1);
+    expect(ins.value).toEqual(null);
+  })
+})
+
+describe('timeout', () => {
+  test('set value', async () => {
+    storage.set('gre', '123456');
+
+    const item = localStorage.getItem(preId + 'gre')
+
+    const value = item!.split('|-|')
+
+    const time = value[0]
+
+    const timeoutTime = Number(time) - 1000 * 60 * 60 * 24
+
+    const _value = timeoutTime.toString() + '|-|' + value[1]
+
+    localStorage.setItem(preId + 'gre',_value)
+
+    const ins = storage.get('gre')
+
+    expect(ins.status).toBe(4)
+
+    expect(ins.value).toBe(null)
   })
 })
